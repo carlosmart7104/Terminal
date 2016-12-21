134210f118480a2374bee5a5f22b0a85eccedae0
carlos jesus martinez diaz <carlos jesus martinez diaz>
2016-12-20 20:24:54 -0600

    se creo el archivo construct.js para mantener  independiente la instancia del objeto

---
+++ js/construct.js
@@ -0,0 +1,5 @@
+// iniciamos la instancia terminal del objeto Terminal con identificador: miTerminal y titulo: 2015. @carlosmart7104.
+var terminal = new Terminal('#miTerminal','Terminal');
+
+// Generamos el mensaje de licencia.
+terminal._console('Terminal v1.0.5. </br> Licencia GPL, usted es libre de copiar, editar y redistribuir este software bajo la misma licencia.</br>Terminal. By: @carlosmart7104. </br> </br>');
